a774430a0d62d73fecce7b1520af0a0e2fc5082f
Erin Young <erin.olde@gmail.com>
2023-01-18 12:26:29 -0700

    bash scripts for downloading databases

--- bin/download_kraken2_db.sh
+++ bin/download_kraken2_db.sh
@@ -5,13 +5,17 @@
 # for downloading kraken2 database for grandeur #
 #################################################
 
+VER="k2_standard_08gb_20221209"
+
 ############################################################
 
 USAGE="
-Downloads Standard 8 Kraken2 database (https://benlangmead.github.io/aws-indexes/k2)
-with wget.
+Downloads $VER pre-built Kraken2 database, a standard 8G Kraken2 database, 
+with wget and extract with tar.
 
-Usage: new_kraken2_db.sh
+(More databases are listed at https://benlangmead.github.io/aws-indexes/k2)
+
+Usage: new_kraken2_db.sh kraken2_db
 "
 
 ############################################################
@@ -29,11 +33,11 @@ mkdir -p $out
 cd $out
 
 echo "$(date): Downloading Kraken2 database"
-wget -c "https://genome-idx.s3.amazonaws.com/kraken/k2_standard_08gb_20221209.tar.gz"
+wget -c "https://genome-idx.s3.amazonaws.com/kraken/$VER.tar.gz"
 
 echo "$(date): Extracting Kraken2 database"
 
-tar -zxvf k2_standard_08gb_20221209.tgz
+tar -zxvf $VER.tar.gz || echo "$(date) : WARNING : could not extract $VER.tar.gz "
 echo "$(date): Finished"
 
 ############################################################
--- bin/download_mash_ref.sh
+++ bin/download_mash_ref.sh
@@ -12,16 +12,22 @@ ver=215
 
 USAGE="
 Downloads a premade mash reference file for refseq v$ver
+from $URL
+
+download_mash_ref.sh mash_v$ver
 "
 
 ############################################################
 
 echo "$USAGE"
 
+# Setting final directory
+if [ -z "$1" ] ; then out=mash_v$ver ; else out=$1 ; fi
+
 echo "$(date): Downloading file"
 
-mkdir mash_v$ver
-cd mash_v$ver
+mkdir $out
+cd $out
 
 wget --continue --show-progress $URL
 
