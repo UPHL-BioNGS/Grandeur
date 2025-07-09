abf286fb86770bf3a52b2ca3b637962e387ba6cb
Moijueh <xre6@cdc.gov>
2025-07-09 15:57:05 -0400

    milestone: integration test part III was successful

--- run_grandeur.sh
+++ run_grandeur.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #$ -cwd # Run job from current directory
-#$ -N grandeur_refactor # Job name
+#$ -N integration_test3 # Job name
 #$ -l h_vmem=128G # Memory requirement
 #$ -l h_rt=04:00:00 # Max runtime (4 hours)
 #$ -pe smp 8 # Request 8 CPUs
@@ -16,4 +16,7 @@ module load singularity
 
 nextflow run /scicomp/home-pure/xre6/Projects/grandeur/main.nf \
 -profile my_test,singularity \
---outdir /scicomp/home-pure/xre6/Projects/grandeur/grandeur_testing/nuclu_identity_refactor_pipeline
+--outdir /scicomp/home-pure/xre6/Projects/grandeur/grandeur_testing/integration_test3_refactor_pipeline \
+--kraken2_db /scicomp/home-pure/xre6/kraken2_database/ \
+--blast_db /scicomp/reference-pure/ncbi-blast-databases \
+--blast_db_type core_nt
